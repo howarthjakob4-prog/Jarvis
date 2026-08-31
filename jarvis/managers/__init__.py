1bb19abc7f6cda445b139d0fb1d7235ddc3deb18
Jakob Howarth <howarthjakob4@gmail.com>
2026-08-31 04:22:46 -0400

    Add Jarvis multi-manager architecture

---
+++ jarvis/managers/__init__.py
@@ -0,0 +1,5 @@
+"""Specialized manager architecture for JARVIS."""
+
+from .manager_system import ManagerSystem, JarvisManager, ManagerRequest
+
+__all__ = ["ManagerSystem", "JarvisManager", "ManagerRequest"]
