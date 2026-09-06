d6848b157f607d31ac5688219585579494f982fb
Jakob Howarth <howarthjakob4@gmail.com>
2026-09-06 08:18:36 -0400

    Expose Jarvis 3D workspace

---
+++ jarvis/ui/__init__.py
@@ -0,0 +1,3 @@
+from jarvis.ui.design_workspace import DesignWorkspace, DesignWorkspaceController, install_design_workspace
+
+__all__ = ["DesignWorkspace", "DesignWorkspaceController", "install_design_workspace"]
